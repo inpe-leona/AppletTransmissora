e424d6d56bb0612e7daf51af04646bc0bd83e210
Admin <Admin@leona-99-02>
2015-01-07 14:07:19 -0200

    versao transmitter atual

--- transmitter/src/main/java/br/leona/transmitter/SwingTransmitter.java
+++ transmitter/src/main/java/br/leona/transmitter/SwingTransmitter.java
@@ -13,57 +13,57 @@ import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
 public class SwingTransmitter extends JFrame {
-	public static Transmitter transmissor = null;
-	private ThreadRTP threadRTP;
+    public static Transmitter transmissor = null;
+    private ThreadRTP threadRTP;
 
-	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				new SwingTransmitter();
-			}
-		});
-	}
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new SwingTransmitter();
+            }
+        });
+    }
 
-	public SwingTransmitter() {
-		setTitle("Transmissor");
-		JPanel panelServer = new JPanel();
-		panelServer
-				.setLayout(new BoxLayout(panelServer, BoxLayout.LINE_AXIS));
-		JLabel namePort = new JLabel("Porta para conex�o TCP: ");
-		final JTextField port = new JTextField();
-		panelServer.add(namePort);
-		panelServer.add(port);
-		JPanel panelButton = new JPanel();
-		panelButton.setLayout(new BoxLayout(panelButton, BoxLayout.LINE_AXIS));
-		final JButton turnOn = new JButton("Ligar");
-		turnOn.setEnabled(true);
-		final JButton turnOff = new JButton("Desligar");
-		turnOff.setEnabled(false);
-		turnOn.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				turnOn.setEnabled(false);
-				turnOff.setEnabled(true);
-				(threadRTP = new ThreadRTP(Integer.parseInt(port.getText()))).execute();// escolhida a port do socket para a conexao TCP, 
-																						 //pode ser qualquer uma, eu tinha escolhido 1234 ao acaso.
-																						// esse numero especifica em qual port o servidor vai estar escutando
-																						//pedidos de conexao tcp de clientes.
-			}
-		});
-		turnOff.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				turnOn.setEnabled(true);
-				turnOff.setEnabled(false);
-				threadRTP.cancel(true);
-				threadRTP = null;
-			}
-		});
-		panelButton.add(turnOn);
-		panelButton.add(turnOff);
-		getContentPane().add(panelServer, BorderLayout.PAGE_START);
-		getContentPane().add(panelButton, BorderLayout.PAGE_END);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(600, 300);
-		setVisible(true);
-	}
+    public SwingTransmitter() {
+        setTitle("Transmissor");
+        JPanel panelServer = new JPanel();
+        panelServer.setLayout(new BoxLayout(panelServer, BoxLayout.LINE_AXIS));
+        JLabel namePort = new JLabel("Porta para conex�o TCP: ");
+        final JTextField port = new JTextField();
+        panelServer.add(namePort);
+        panelServer.add(port);
+        JPanel panelButton = new JPanel();
+        panelButton.setLayout(new BoxLayout(panelButton, BoxLayout.LINE_AXIS));
+        final JButton turnOn = new JButton("Ligar");
+        turnOn.setEnabled(true);
+        final JButton turnOff = new JButton("Desligar");
+        turnOff.setEnabled(false);
+        turnOn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                turnOn.setEnabled(false);
+                turnOff.setEnabled(true);
+        // escolhida a port do socket para a conexao TCP, 
+                (threadRTP = new ThreadRTP(Integer.parseInt(port.getText()))).execute();
+        //pode ser qualquer uma, eu tinha escolhido 1234 ao acaso.
+        // esse numero especifica em qual port o servidor vai estar escutando
+        //pedidos de conexao tcp de clientes.
+            }
+        });
+        turnOff.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                turnOn.setEnabled(true);
+                turnOff.setEnabled(false);
+                threadRTP.cancel(true);
+                threadRTP = null;
+            }
+        });
+        panelButton.add(turnOn);
+        panelButton.add(turnOff);
+        getContentPane().add(panelServer, BorderLayout.PAGE_START);
+        getContentPane().add(panelButton, BorderLayout.PAGE_END);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(600, 300);
+        setVisible(true);
+    }
 
 }
--- transmitter/src/main/java/br/leona/transmitter/Transmitter.java
+++ transmitter/src/main/java/br/leona/transmitter/Transmitter.java
@@ -30,237 +30,233 @@ import javax.media.rtp.SessionAddress;
 import javax.media.rtp.rtcp.SourceDescription;
 
 public class Transmitter {
-	private MediaLocator locator;
-	private String hostAddress;
-	private int port;
-	private Processor processor = null;
-	private RTPManager[] messageRTP;
-	private DataSource dataOutput = null;
+    private MediaLocator locator;
+    private String hostAddress;
+    private int port;
+    private Processor processor = null;
+    private RTPManager[] messageRTP;
+    private DataSource dataOutput = null;
 
-	public Transmitter(MediaLocator locator, String hostAddress, int port,
-			Format format) {
-		this.locator = locator;
-		this.hostAddress = hostAddress;
-		this.port = port;
-	}
+    public Transmitter(MediaLocator locator, String hostAddress, int port,
+                       Format format) {
+        this.locator = locator;
+        this.hostAddress = hostAddress;
+        this.port = port;
+    }
 
-	public synchronized String start() {
-		String result;
-		result = createProcessor();
-		if (result != null)
-			return result;
-		result = createTransmitter();
-		if (result != null) {
-			processor.close();
-			processor = null;
-			return result;
-		}
-		processor.start();
-		return null;
-	}
+    public synchronized String start() {
+        String result;
+        result = createProcessor();
+        if (result != null)
+            return result;
+        result = createTransmitter();
+        if (result != null) {
+            processor.close();
+            processor = null;
+            return result;
+        }
+        processor.start();
+        return null;
+    }
 
-	public void stop() {
-		synchronized (this) {
-			if (processor != null) {
-				processor.stop();
-				processor.close();
-				processor = null;
-				for (int i = 0; i < messageRTP.length; i++) {
-					messageRTP[i].removeTargets("Sess�o terminada.");
-					messageRTP[i].dispose();
-				}
-			}
-		}
-	}
+    public void stop() {
+        synchronized (this) {
+            if (processor != null) {
+                processor.stop();
+                processor.close();
+                processor = null;
+                for (int i = 0; i < messageRTP.length; i++) {
+                    messageRTP[i].removeTargets("Sessao terminada.");
+                    messageRTP[i].dispose();
+                }
+            }
+        }
+    }
 
-	private String createProcessor() {
-		if (locator == null) {
-			return "Locator � nulo";
-		}
-		DataSource sataSource;
-		try {
-			sataSource = javax.media.Manager.createDataSource(locator);
-		} catch (Exception e) {
-			return "Couldn't create DataSource";
-		}
-		try {
-			processor = javax.media.Manager.createProcessor(sataSource);
-		} catch (NoProcessorException npe) {
-			return "Couldn't create processor";
-		} catch (IOException ioe) {
-			return "IOException creating processor";
-		}
-		boolean result = waitForState(processor, Processor.Configured);
-		if (result == false)
-			return "Couldn't configure processor";
-		TrackControl[] tracks = processor.getTrackControls();
-		if (tracks == null || tracks.length < 1)
-			return "Couldn't find tracks in processor";
-		ContentDescriptor cd = new ContentDescriptor(ContentDescriptor.RAW_RTP);
-		processor.setContentDescriptor(cd);
-		Format supported[];
-		Format chosen;
-		boolean atLeastOneTrack = false;
-		for (int i = 0; i < tracks.length; i++) {
-			Format format = tracks[i].getFormat();
-			if (tracks[i].isEnabled()) {
-				supported = tracks[i].getSupportedFormats();
-				if (supported.length > 0) {
-					if (supported[0] instanceof VideoFormat) {
-						chosen = checkForVideoSizes(tracks[i].getFormat(),
-								supported[0]);
-					} else
-						chosen = supported[0];
-					tracks[i].setFormat(chosen);
-					System.err
-							.println("Track " + i + " is set to transmit as:");
-					System.err.println("  " + chosen);
-					atLeastOneTrack = true;
-				} else
-					tracks[i].setEnabled(false);
-			} else
-				tracks[i].setEnabled(false);
-		}
-		if (!atLeastOneTrack)
-			return "Couldn't set any of the tracks to a valid RTP format";
-		result = waitForState(processor, Controller.Realized);
-		if (result == false)
-			return "Couldn't realize processor";
-		setJPEGQuality(processor, 0.5f);
-		dataOutput = processor.getDataOutput();
-		return null;
-	}
+    private String createProcessor() {
+        if (locator == null) {
+            return "Locator null";
+        }
+        DataSource sataSource;
+        try {
+            sataSource = javax.media.Manager.createDataSource(locator);
+        } catch (Exception e) {
+            return "Couldn't create DataSource";
+        }
+        try {
+            processor = javax.media.Manager.createProcessor(sataSource);
+        } catch (NoProcessorException npe) {
+            return "Couldn't create processor";
+        } catch (IOException ioe) {
+            return "IOException creating processor";
+        }
+        boolean result = waitForState(processor, Processor.Configured);
+        if (result == false)
+            return "Couldn't configure processor";
+        TrackControl[] tracks = processor.getTrackControls();
+        if (tracks == null || tracks.length < 1)
+            return "Couldn't find tracks in processor";
+        ContentDescriptor cd = new ContentDescriptor(ContentDescriptor.RAW_RTP);
+        processor.setContentDescriptor(cd);
+        Format supported[];
+        Format chosen;
+        boolean atLeastOneTrack = false;
+        for (int i = 0; i < tracks.length; i++) {
+            Format format = tracks[i].getFormat();
+            if (tracks[i].isEnabled()) {
+                supported = tracks[i].getSupportedFormats();
+                if (supported.length > 0) {
+                    if (supported[0] instanceof VideoFormat) {
+                        chosen = checkForVideoSizes(tracks[i].getFormat(),
+                                                    supported[0]);
+                    } else
+                        chosen = supported[0];
+                    tracks[i].setFormat(chosen);
+                    System.err.println("Track " + i + " is set to transmit as:");
+                    System.err.println("  " + chosen);
+                    atLeastOneTrack = true;
+                } else
+                    tracks[i].setEnabled(false);
+            } else
+                tracks[i].setEnabled(false);
+        }
+        if (!atLeastOneTrack)
+                return "Couldn't set any of the tracks to a valid RTP format";
+        result = waitForState(processor, Controller.Realized);
+        if (result == false)
+                return "Couldn't realize processor";
+        setJPEGQuality(processor, 0.5f);
+        dataOutput = processor.getDataOutput();
+        return null;
+    }
 
-	private String createTransmitter() {
-		PushBufferDataSource pbds = (PushBufferDataSource) dataOutput;
-		PushBufferStream pbss[] = pbds.getStreams();
-		messageRTP = new RTPManager[pbss.length];
-		SessionAddress localAddr, destAddr;
-		InetAddress ipAddr;
-		SendStream sendStream;
-		int port;
-		SourceDescription srcDesList[];
-		for (int i = 0; i < pbss.length; i++) {
-			try {
-				messageRTP[i] = RTPManager.newInstance();
-				port = this.port + 2 * i;
-				ipAddr = InetAddress.getByName(hostAddress);
-				localAddr = new SessionAddress(InetAddress.getLocalHost(), port);
-				destAddr = new SessionAddress(ipAddr, port);
-				messageRTP[i].initialize(localAddr);
-				messageRTP[i].addTarget(destAddr);
-				System.err.println("Created RTP session: " + hostAddress + " "
-						+ port);
-				sendStream = messageRTP[i].createSendStream(dataOutput, i);
-				sendStream.start();
-			} catch (Exception e) {
-				return e.getMessage();
-			}
-		}
-		return null;
-	}
+    private String createTransmitter() {
+        PushBufferDataSource pbds = (PushBufferDataSource) dataOutput;
+        PushBufferStream pbss[] = pbds.getStreams();
+        messageRTP = new RTPManager[pbss.length];
+        SessionAddress localAddr, destAddr;
+        InetAddress ipAddr;
+        SendStream sendStream;
+        int port;
+        SourceDescription srcDesList[];
+        for (int i = 0; i < pbss.length; i++) {
+            try {
+                messageRTP[i] = RTPManager.newInstance();
+                port = this.port + 2 * i;
+                ipAddr = InetAddress.getByName(hostAddress);
+                localAddr = new SessionAddress(InetAddress.getLocalHost(), port);
+                destAddr = new SessionAddress(ipAddr, port);
+                messageRTP[i].initialize(localAddr);
+                messageRTP[i].addTarget(destAddr);
+                System.err.println("Created RTP session: " + hostAddress + " "
+                                  + port);
+                sendStream = messageRTP[i].createSendStream(dataOutput, i);
+                sendStream.start();
+            } catch (Exception e) {
+                return e.getMessage();
+            }
+        }
+        return null;
+    }
 
-	Format checkForVideoSizes(Format original, Format supported) {
-		int width, height;
-		Dimension size = ((VideoFormat) original).getSize();
-		Format jpegFmt = new Format(VideoFormat.JPEG_RTP);
-		Format h263Fmt = new Format(VideoFormat.H263_RTP);
-		
-		if (supported.matches(jpegFmt)) {
-			width = (size.width % 8 == 0 ? size.width
-					: (int) (size.width / 8) * 8);
-			height = (size.height % 8 == 0 ? size.height
-					: (int) (size.height / 8) * 8);
-		} else if (supported.matches(h263Fmt)) {
-			if (size.width < 128) {
-				width = 128;
-				height = 96;
-			} else if (size.width < 176) {
-				width = 176;
-				height = 144;
-			} else {
-				width = 352;
-				height = 288;
-			}
-		} else {
+    Format checkForVideoSizes(Format original, Format supported) {
+        int width, height;
+        Dimension size = ((VideoFormat) original).getSize();
+        Format jpegFmt = new Format(VideoFormat.JPEG_RTP);
+        Format h263Fmt = new Format(VideoFormat.H263_RTP);
 
-			return supported;
-		}
-		
-		return (new VideoFormat(null, new Dimension(width, height),
-				Format.NOT_SPECIFIED, null, Format.NOT_SPECIFIED))
-				.intersects(supported);
-	}
+        if (supported.matches(jpegFmt)) {
+            width = (size.width % 8 == 0 ? size.width
+                    : (int) (size.width / 8) * 8);
+            height = (size.height % 8 == 0 ? size.height
+                    : (int) (size.height / 8) * 8);
+        } else if (supported.matches(h263Fmt)) {
+            if (size.width < 128) {
+                width = 128;
+                height = 96;
+            } else if (size.width < 176) {
+                width = 176;
+                height = 144;
+            } else {
+                width = 352;
+                height = 288;
+            }
+        } else {
+            return supported;
+        }
 
-	void setJPEGQuality(Player p, float val) {
-		Control cs[] = p.getControls();
-		QualityControl qc = null;
-		VideoFormat formato = new VideoFormat(VideoFormat.JPEG);
-		for (int i = 0; i < cs.length; i++) {
-			if (cs[i] instanceof QualityControl && cs[i] instanceof Owned) {
-				Object objeto = ((Owned) cs[i]).getOwner();
-				if (objeto instanceof Codec) {
-					Format fmts[] = ((Codec) objeto)
-							.getSupportedOutputFormats(null);
-					for (int j = 0; j < fmts.length; j++) {
-						if (fmts[j].matches(formato)) {
-							qc = (QualityControl) cs[i];
-							qc.setQuality(val);
-							System.err.println("- Qualidade configurada para " + val
-									+ " em " + qc);
-							break;
-						}
-					}
-				}
-				if (qc != null)
-					break;
-			}
-		}
-	}
+        return (new VideoFormat(null, new Dimension(width, height),
+                Format.NOT_SPECIFIED, null, Format.NOT_SPECIFIED)).intersects(supported);
+    }
 
-	private Integer stateLock = new Integer(0);
-	private boolean failed = false;
+    void setJPEGQuality(Player p, float val) {
+        Control cs[] = p.getControls();
+        QualityControl qc = null;
+        VideoFormat formato = new VideoFormat(VideoFormat.JPEG);
+        for (int i = 0; i < cs.length; i++) {
+            if (cs[i] instanceof QualityControl && cs[i] instanceof Owned) {
+                Object objeto = ((Owned) cs[i]).getOwner();
+                if (objeto instanceof Codec) {
+                    Format fmts[] = ((Codec) objeto).getSupportedOutputFormats(null);
+                    for (int j = 0; j < fmts.length; j++) {
+                        if (fmts[j].matches(formato)) {
+                            qc = (QualityControl) cs[i];
+                            qc.setQuality(val);
+                            System.err.println("- Qualidade configurada para " + val
+                                              + " em " + qc);
+                            break;
+                        }
+                    }
+                }
+                if (qc != null)
+                    break;
+            }
+        }
+    }
 
-	Integer getStateLock() {
-		return stateLock;
-	}
+    private Integer stateLock = new Integer(0);
+    private boolean failed = false;
 
-	void setFailed() {
-		failed = true;
-	}
+    Integer getStateLock() {
+        return stateLock;
+    }
 
-	private synchronized boolean waitForState(Processor p, int state) {
-		p.addControllerListener(new StateListener());
-		failed = false;
-		if (state == Processor.Configured) {
-			p.configure();
-		} else if (state == Processor.Realized) {
-			p.realize();
-		}
-		while (p.getState() < state && !failed) {
-			synchronized (getStateLock()) {
-				try {
-					getStateLock().wait();
-				} catch (InterruptedException ie) {
-					return false;
-				}
-			}
-		}
-		if (failed)
-			return false;
-		else
-			return true;
-	}
+    void setFailed() {
+        failed = true;
+    }
 
-	class StateListener implements ControllerListener {
-		public void controllerUpdate(ControllerEvent ce) {
-			if (ce instanceof ControllerClosedEvent)
-				setFailed();
-			if (ce instanceof ControllerEvent) {
-				synchronized (getStateLock()) {
-					getStateLock().notifyAll();
-				}
-			}
-		}
-	}
+    private synchronized boolean waitForState(Processor p, int state) {
+        p.addControllerListener(new StateListener());
+        failed = false;
+        if (state == Processor.Configured) {
+            p.configure();
+        } else if (state == Processor.Realized) {
+            p.realize();
+        }
+        while (p.getState() < state && !failed) {
+            synchronized (getStateLock()) {
+                try {
+                    getStateLock().wait();
+                } catch (InterruptedException ie) {
+                    return false;
+                }
+            }
+        }
+        if (failed)
+            return false;
+        else
+            return true;
+    }
+
+    class StateListener implements ControllerListener {
+        public void controllerUpdate(ControllerEvent ce) {
+            if (ce instanceof ControllerClosedEvent)
+                setFailed();
+            if (ce instanceof ControllerEvent) {
+                synchronized (getStateLock()) {
+                    getStateLock().notifyAll();
+                }
+            }
+        }
+    }
 }
